26296ea078f698e274c1df34befb620e96d51da4
JuanCarlosCarbajosaGarcia <juanc.cargar@educa.jcyl.es>
2026-01-30 09:56:26 +0100

    V1.0

--- src/GestorAlumnos.java
+++ src/GestorAlumnos.java
@@ -1,28 +1,56 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class GestorAlumnos {
     public static void main(String[] args) {
-        Map<String, Double> Notas = new HashMap<>();
+        Map<String, List> alumnos = new HashMap<>();
+        String FAlumnos = "Alumnos.txt";
+        String Resultados = "Resultados.txt";
 
-        try (BufferedReader reader = new BufferedReader(new FileReader("Alumnos.txt"))){
-            String linea;
-
-            while ((linea = reader.readLine()) != null){
-                String[]data = linea.split(";");
+        //leer el archivo
+        try (BufferedReader reader = new BufferedReader(new FileReader(FAlumnos));){
+            String line;
+            while ((line = reader.readLine()) != null){
+                String[] data = line.split(";");
 
                 if(data.length==2){
-                    String nombre=data[0];
-                    Double nota=Double.parseDouble(data[1]);
+                    String Nombre = data[0];
+                    double Nota = Double.parseDouble(data[1]);
+
+                    alumnos.putIfAbsent(Nombre, new ArrayList());
+                    alumnos.get(Nombre).add(Nota);
                 }
             }
-
         }catch (Exception e){
             System.out.println("error al leer el archivo: " + e.getMessage());
+            return;
         }
 
-        System.out.println("Notas: " + Notas);
+        //Nota media
+        Map<String, Double> medias = new HashMap<>();
+
+        for(String alumno : alumnos.keySet()){
+            List<Double> notas = alumnos.get(alumno);
+            double suma = 0;
+            for(double nota : notas){
+                suma += nota;
+            }
+            double media = suma / notas.size();
+            medias.put(alumno, media);
+        }
+
+        //Alumnos aprovados
+        Map<String, Double> aprobados = new HashMap<>();
+
+        for(String alumno : alumnos.keySet()){
+            if (medias.get(alumno)>= 5){
+                aprobados.put(alumno, medias.get(alumno));
+            }
+        }
+
     }
 }
